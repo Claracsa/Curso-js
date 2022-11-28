ea39f35e242e5b026a383c21a647d98016d46cb5
claracsa <claracsa1@gmail.com>
2022-11-27 21:55:58 -0300

    modificação aula 11 ex011

--- Aula 11/Ex011/JavaScript.js
+++ Aula 11/Ex011/JavaScript.js
@@ -1,12 +1,25 @@
 function pesquisar() {
-  var txtp = window.document.querySelector('div#txtpais');
+  var txtp = window.document.querySelector('div#txtpais').value;
   var res = window.document.querySelector('div#res');
 
   res.innerHTML = `<p>Vivendo no país ${txtp}</p>`;
 
-  if ((txtp = 'Brasil')) {
+  if (txtp = 'Brasil') {
     res.innerHTML += 'Você é <strong>brasileiro</strong>.';
   } else {
     res.innerHTML += 'Você é <strong>estrangeiro</strong>.';
   }
 }
+
+/*function pesquisar() {
+  var txtp = window.document.getElementById('txtpais').value;
+  var res = window.document.querySelector('div#res');
+
+  res.innerHTML = `<p>Vivendo no país ${txtp}</p>`;
+
+  if (txtp = 'Brasil') {
+    res.innerHTML += 'Você é <strong>brasileiro</strong>.';
+  } else {
+    res.innerHTML += 'Você é <strong>estrangeiro</strong>.';
+  }
+}/*
