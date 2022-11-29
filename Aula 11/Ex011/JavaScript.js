6a4032f4c8cabed11adaceb1a4aa0954dc016aba
claracsa <claracsa1@gmail.com>
2022-11-29 16:15:11 -0300

    alteração de frase.

--- Aula 11/Ex011/JavaScript.js
+++ Aula 11/Ex011/JavaScript.js
@@ -2,7 +2,7 @@ function pesquisar() {
   var txtp = window.document.getElementById('txtpais').value;
   var res = window.document.querySelector('div#res');
 
-  res.innerHTML = `<p>Vivendo no país ${txtp}</p>`;
+  res.innerHTML = `<p>Voê nasceu no país ${txtp}</p>`;
 
   if (txtp == 'Brasil') {
     res.innerHTML += 'Você é <strong>brasileiro</strong>.';
