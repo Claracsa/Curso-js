7c964d20300b071b01c4b6133c501d9059b29bc6
claracsa <claracsa1@gmail.com>
2022-12-04 19:03:15 -0300

    continuação do exercício prático da Aula 12

--- Aula 12 EX/Ex015/JavaScript.js
+++ Aula 12 EX/Ex015/JavaScript.js
@@ -1,20 +1,20 @@
 function carregar() {
-    var msg = window.document.querySelector('#mensagem');
-    var img = window.document.querySelector('#imagem');
-    /*var img = window.document.getElementById('imagem');*/
+  var msg = window.document.querySelector('#mensagem');
+  var img = window.document.querySelector('#imagem');
+  /*var img = window.document.getElementById('imagem');*/
 
-    var data = new Date();
-    var hora = data.getHours();
+  var data = new Date();
+  var hora = data.getHours();
 
-    var hora = 22;
-    
-    msg.innerHTML = `Agora são ${hora} horas.`;
+  var hora = 22;
 
-    if (hora >= 0 && hora < 12) {
-        img.scr = '/Aula 12 EX/Ex015/manha.png';
-    } else if (hora >= 12 && hora <= 18) {
-        img.src = '/Aula 12 EX/Ex015/tarde.png';
-    } else {
-        img.scr = '/Aula 12 EX/Ex015/noite.png';
-    }
+  msg.innerHTML = `Agora são ${hora} horas.`;
+
+  if (hora >= 0 && hora < 12) {
+    img.scr = 'manha.png';
+  } else if (hora >= 12 && hora < 18) {
+    img.src = 'tarde.png';
+  } else {
+    img.scr = 'noite.png';
+  }
 }
