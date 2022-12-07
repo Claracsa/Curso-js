a4c4dd22edfe5cb636e8e8385deba49b9b1d1f08
claracsa <claracsa1@gmail.com>
2022-12-07 20:08:04 -0300

    finalização do ex015, horarios do dia com fotos

--- Aula 12 EX/Ex015/JavaScript.js
+++ Aula 12 EX/Ex015/JavaScript.js
@@ -1,20 +1,26 @@
 function carregar() {
   var msg = window.document.querySelector('#mensagem');
   var img = window.document.querySelector('#imagem');
-  /*var img = window.document.getElementById('imagem');*/
+  //var img = window.document.getElementById('imagem');
 
   var data = new Date();
   var hora = data.getHours();
 
-  var hora = 22;
 
   msg.innerHTML = `Agora são ${hora} horas.`;
 
-  if (hora >= 0 && hora < 12) {
-    img.scr = 'manha.png';
-  } else if (hora >= 12 && hora < 18) {
-    img.src = 'tarde.png';
-  } else {
-    img.scr = 'noite.png';
-  }
+if (hora >= 0 && hora < 12) {
+  //BOM DIA #f5dd8f
+  img.src = 'manha.png';
+  document.body.style.background = '#f5dd8f';
+} else if (hora >= 12 && hora < 18) {
+  //BOA TARDE #c69969
+  img.src = 'tarde.png';
+  document.body.style.background = '#c69969';
+} else {
+  //BOA NOITE #042d3f
+  img.src = 'noite.png';
+  document.body.style.background = '#042d3f';
+}
+
 }
