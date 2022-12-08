28ff5a42fdfebf4308c8d99b9de22574d838e5eb
claracsa <claracsa1@gmail.com>
2022-12-08 15:42:50 -0300

    finalização da aula 12

--- Aula 12 EX/Ex015/JavaScript.js
+++ Aula 12 EX/Ex015/JavaScript.js
@@ -6,21 +6,19 @@ function carregar() {
   var data = new Date();
   var hora = data.getHours();
 
-
   msg.innerHTML = `Agora são ${hora} horas.`;
 
-if (hora >= 0 && hora < 12) {
-  //BOM DIA #f5dd8f
-  img.src = 'manha.png';
-  document.body.style.background = '#f5dd8f';
-} else if (hora >= 12 && hora < 18) {
-  //BOA TARDE #c69969
-  img.src = 'tarde.png';
-  document.body.style.background = '#c69969';
-} else {
-  //BOA NOITE #042d3f
-  img.src = 'noite.png';
-  document.body.style.background = '#042d3f';
-}
-
+  if (hora >= 0 && hora < 12) {
+    //BOM DIA #f5dd8f
+    img.src = 'manha.png';
+    document.body.style.background = '#f5dd8f';
+  } else if (hora >= 12 && hora < 18) {
+    //BOA TARDE #c69969
+    img.src = 'tarde.png';
+    document.body.style.background = '#c69969';
+  } else {
+    //BOA NOITE #042d3f
+    img.src = 'noite.png';
+    document.body.style.background = '#042d3f';
+  }
 }
